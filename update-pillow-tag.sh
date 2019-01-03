60223b47e2fbf1f51d2f120c4c8b4b6b2f34910f
Andrew Murray <radarhere@users.noreply.github.com>
2019-01-04 10:38:02 +1100

    Added script to update Pillow

---
+++ update-pillow-tag.sh
@@ -0,0 +1,14 @@
+if [ $# -eq 0 ]; then
+    echo "Usage: update-pillow-tag.sh [[release tag]]"
+    exit
+fi
+
+git checkout master
+git submodule init
+git submodule update Pillow
+cd Pillow
+git fetch --all
+git checkout $1 || exit
+cd ..
+git commit -m "Pillow -> $1" Pillow
+git push
