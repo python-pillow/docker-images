210a1985b5c6092daff242050e61373b00180ce4
Andrew Murray <radarhere@users.noreply.github.com>
2019-07-08 19:51:34 +1000

    Added Debian Buster

---
+++ debian-buster-x86/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /vpy/bin/activate
+cd /Pillow
+export DISPLAY=:99.0
+make clean
+make install-coverage
+/usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
+
+source /vpy3/bin/activate
+make clean
+make install-coverage
+/usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
