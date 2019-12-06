36abf86bdbb1ae9a8e62d0b4dd7b9b4e8607689f
Andrew Murray <radarhere@users.noreply.github.com>
2019-12-06 23:53:26 +1100

    Removed Python 2

--- arch/test.sh
+++ arch/test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-source /vpy/bin/activate
-cd /Pillow
-make clean
-make install-coverage
-QT_QPA_PLATFORM=offscreen /usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
-
 source /vpy3/bin/activate
+cd /Pillow
 make clean
 make install-coverage
 QT_QPA_PLATFORM=offscreen /usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
