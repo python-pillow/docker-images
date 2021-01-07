130c4877deef0b0407a36742e40605616f145061
Eric Soroos <eric-github@soroos.net>
2021-01-07 15:28:16 +0100

    remove old bits of the test.sh, add some sample calls

--- ubuntu-20.04-focal-amd64-valgrind/test.sh
+++ ubuntu-20.04-focal-amd64-valgrind/test.sh
@@ -3,12 +3,14 @@ source /vpy3/bin/activate
 cd /Pillow
 make clean
 make install-coverage
-#PYTHONMALLOC=malloc valgrind pytest -vx --cov PIL --cov-report term Tests 2>&1 | grep -B10 -A3 decode.c
-# --show-leak-kinds=definite
+
 PYTHONMALLOC=malloc valgrind --suppressions=/depends/python.supp --leak-check=no \
             --log-file=/tmp/valgrind-output \
             python -m pytest --no-memcheck -vv --valgrind --valgrind-log=/tmp/valgrind-output
 
-if [ "$?" -eq 0 ]; then
-    exit 1;
-fi;
+# To run one test in the image:
+# make bash
+# PYTHONMALLOC=malloc valgrind --leak-check=no --suppressions=/depends/python.supp  pytest -vv --valgrind --no-memcheck Tests/test_file_eps.py
+
+# to run one test with GDB support:
+# PYTHONMALLOC=malloc valgrind --vgdb-error=0 --suppressions=../../test/python.supp pytest Tests/test_file_libtiff.py::TestFileLibTiff::test_custom_metadata
