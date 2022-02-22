a7735bc4c51d57a7e74f44ac239043849e005dd8
Andrew Murray <radarhere@users.noreply.github.com>
2022-02-22 17:58:29 +1100

    Added further dependencies

--- gentoo/test.sh
+++ gentoo/test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source /vpy3/bin/activate
 cd /Pillow
-pip install pytest
 make clean
 make install-coverage
 python3 -c "from PIL import Image"
-QT_QPA_PLATFORM=offscreen /usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
+/usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
